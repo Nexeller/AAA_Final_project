c2c0d47b69355dc32eaa9ac9babefedf4665b689
Nexeller <55556645+Nexeller@users.noreply.github.com>
2023-12-05 01:26:08 +0300

    Pizza project

---
+++ final_project.py
@@ -0,0 +1,110 @@
+import click
+import random
+import functools
+
+
+class BasePizza:
+    """Представляет общую пиццу с указанием размера, названия и ингредиентов."""
+
+    def __init__(self, size='L'):
+        if size.upper() not in ['L', 'XL']:
+            raise ValueError("NO SUCH SIZE")
+        self.size = size.upper()
+        self.ingredients = []
+
+    def add_ingredient(self, ingredient):
+        self.ingredients.append(ingredient)
+
+    def get_description(self):
+        return f"{self.__class__.__name__} ({self.size}) with {' '.join(self.ingredients)}"
+
+    def dict(self):
+        return {
+            "type": self.__class__.__name__,
+            "size": self.size,
+            "ingredients": self.ingredients
+        }
+
+
+class Margherita(BasePizza):
+    """Класс пиццы Маргарита, подкласс BasePizza."""
+
+    def __init__(self, size='L'):
+        super().__init__(size)
+        self.add_ingredient('tomato sauce')
+        self.add_ingredient('mozzarella')
+        self.add_ingredient('basil')
+
+
+class Pepperoni(BasePizza):
+    """Класс пиццы Пепперони, подкласс BasePizza."""
+
+    def __init__(self, size='L'):
+        super().__init__(size)
+        self.add_ingredient('tomato sauce')
+        self.add_ingredient('mozzarella')
+        self.add_ingredient('pepperoni')
+
+
+class Hawaiian(BasePizza):
+    """Класс пиццы Гавайская, подкласс BasePizza."""
+
+    def __init__(self, size='L'):
+        super().__init__(size)
+        self.add_ingredient('tomato sauce')
+        self.add_ingredient('mozzarella')
+        self.add_ingredient('ham')
+        self.add_ingredient('pineapple')
+
+
+def log_execution_time(func):
+    """Декоратор для логирования времени выполнения функции."""
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        """Обертка функции с логированием времени выполнения."""
+        start_time = random.randint(1, 5)
+        result = func(*args, **kwargs)
+        end_time = random.randint(1, 5)
+        print(f"Function '{func.__name__}' be ready in {end_time - start_time} hour(s)")
+        return result
+
+    return wrapper
+
+
+@click.group()
+def cli():
+    """Выбираем пиццу из нашего меню."""
+    pass
+
+
+@click.command()
+def menu():
+    """Выводит меню наших пицц."""
+    for pizza_class in [Margherita, Pepperoni, Hawaiian]:
+        pizza = pizza_class()
+        click.echo(pizza.get_description())
+
+
+@click.command()
+@click.argument('type')
+@click.option('--size', default='L', help='Size of the pizza (L, XL)')
+@log_execution_time
+def order(type, size):
+    """Обрабатывает заказ пиццы."""
+    pizza_classes = {'margherita': Margherita, 'pepperoni': Pepperoni, 'hawaiian': Hawaiian}
+    try:
+        pizza = pizza_classes[type.lower()](size)
+        click.echo(f"YOUR ORDER: {pizza.get_description()}")
+        click.echo(pizza.dict())
+    except KeyError:
+        click.echo("NO SUCH TYPE OF PIZZA")
+    except ValueError as e:
+        click.echo(e)
+
+
+cli.add_command(menu)
+cli.add_command(order)
+
+if __name__ == '__main__':
+    cli()
